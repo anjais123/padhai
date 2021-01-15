91059005e83e7121435b09f6023bae7ecd493e53
Anurag Jaiswal <anjais@microsoft.com>
2021-01-16 01:27:32 +0530

    add code

--- register_files/userLogin.js
+++ register_files/userLogin.js
@@ -2,11 +2,10 @@ function signUpNewUser(){
     var uname = document.getElementById("userName").value;
     var pwd = document.getElementById("password").value;
 
-    firebase.auth().createUserWithEmailAndPassword(uname, pwd)
-  .then((user) => {
-    alert("New User Registerd");
-  })
-  .catch((error) => {
+    firebase.auth().createUserWithEmailAndPassword(uname, pwd).then((user) => {
+    var ran = "New User Registered";
+    alert(ran);
+  }).catch((error) => {
     var errorCode = error.code;
     var errorMessage = error.message;
     alert("Unable to register. Please try again");
@@ -17,14 +16,16 @@ function signInWithEmailPassword() {
     var email = document.getElementById("loginId").value;
     var password = document.getElementById("loginPwd").value;
     // [START auth_signin_password]
-    firebase.auth().signInWithEmailAndPassword(email, password)
-      .then((user) => {
-          alert("User logged in successfully")
-      })
-      .catch((error) => {
+    firebase.auth().signInWithEmailAndPassword(email, password).then((user) => {
+         // alert("User logged in successfully");
+         if(user)
+          document.getElementById('labelName').innerHTML = "Welcome " + user.uid;
+         else 
+          document.getElementById('labelName').innerHTML = "user";
+      }).catch((error) => {
         var errorCode = error.code;
         var errorMessage = error.message;
-        alert("Either emailId/password is incorrect");
+        alert("Either emailId/password is not correct");
       });
-    // [END auth_signin_password]
+
   }
